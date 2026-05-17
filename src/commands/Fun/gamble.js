eb31ad43dd949a198907eb4c00d52e8ffb897e43
exemnn1 <silasandrocky@gmail.com>
2026-05-17 16:48:39 -0600

    Update gamble.js

--- src/commands/Fun/gamble.js
+++ src/commands/Fun/gamble.js
@@ -1,1 +1,20 @@
+async execute(interaction, config, client) {
+  try {
+    await InteractionHelper.safeReply(interaction, {
+      embeds: [
+        errorEmbed(
+          "Under Development",
+          "This command is currently under development."
+        )
+      ]
+    });
 
+    return;
+  } catch (error) {
+    logger.error('Gamble command error:', error);
+    await handleInteractionError(interaction, error, {
+      commandName: 'gamble',
+      source: 'gamble_command'
+    });
+  }
+},
