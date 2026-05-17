2d6d15779c8dd4b75aaf551177670e25c06db54d
exemnn1 <silasandrocky@gmail.com>
2026-05-17 16:44:00 -0600

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -27,7 +27,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "This is the main Discord bot for Blue Helm Studios Discord servers",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 4, 
       },
     ],
   },
